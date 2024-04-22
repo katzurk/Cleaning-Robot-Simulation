9d7a864a1c734e5f1af250ac61602b09c45a3ef6
kkanicka <01187237@pw.edu.pl>
2024-04-22 17:46:57 +0200

    added class visualiser

---
+++ visualiser.cpp
@@ -0,0 +1,27 @@
+#include "visualiser.h"
+
+Visualizer::Visualizer(QWidget* parent) : QGraphicsView(parent) {
+    scene = new QGraphicsScene(this);
+    setScene(scene);
+}
+
+void Visualizer::setRoom(const Room& room) {
+    this->room = room;
+    scene->clear(); // Clear the scene
+    // Draw the room
+    scene->addRect(0, 0, room.getLength(), room.getWidth(), QPen(Qt::black), QBrush(Qt::white));
+    // Draw the furniture
+    for (const auto& furniture : room.getFurniture()) {
+        scene->addRect(furniture.get_coordinates()[0], furniture.get_coordinates()[1],
+            furniture.getLength(), furniture.getWidth(), QPen(Qt::black), QBrush(Qt::blue));
+    }
+    update(); // Trigger repaint
+}
+
+void Visualizer::setRobot(const Robot& robot) {
+    this->robot = robot;
+    // Draw the robot
+    scene->addRect(robot.get_coordinates()[0], robot.get_coordinates()[1],
+        robot.get_size(), robot.get_size(), QPen(Qt::black), QBrush(Qt::red));
+    update(); // Trigger repaint
+}
