688fbd03f3da886bcb730e09d7d68fa986df150b
kkanicka <01187237@pw.edu.pl>
2024-05-06 18:01:39 +0200

    more minor fixes

--- simulation.cpp
+++ simulation.cpp
@@ -21,11 +21,8 @@ void Simulation::setRoom(const Room& room) {
 void Simulation::setRobot(const Robot& robot) {
     this->robot = robot;
     // Draw the robot
-    robotObject = new QGraphicsRectItem(robot.get_coordinates()[0], robot.get_coordinates()[1],
-        robot.get_length(), robot.get_width());
-    robotObject->setPen(QPen(Qt::black));
-    robotObject->setBrush(QBrush(Qt::red));
-    addItem(robotObject);
+    robotObject = addRect(robot.get_coordinates()[0], robot.get_coordinates()[1],
+        robot.get_length(), robot.get_width(), QPen(Qt::black), QBrush(Qt::red));
 }
 
 void Simulation::generatePath() {
@@ -53,10 +50,7 @@ void Simulation::moveRobot() {
         robotObject = nullptr;
     }
     // Create a new robotObject with new coordinates
-    robotObject = new QGraphicsRectItem(x, y, robot.get_length(), robot.get_width());
-    robotObject->setPen(QPen(Qt::black));
-    robotObject->setBrush(QBrush(Qt::red));
-    addItem(robotObject);
+    robotObject = addRect(x, y, robot.get_length(), robot.get_width(), QPen(Qt::black), QBrush(Qt::red));
 
     currentPositionId++;
 }
