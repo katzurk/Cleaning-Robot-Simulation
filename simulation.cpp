72f4d90cceaa3a6f16231b304f888d63b7d959f1
afilinko <01194269@pw.edu.pl>
2024-05-17 11:52:31 +0200

    added dust on the screen

--- simulation.cpp
+++ simulation.cpp
@@ -11,6 +11,13 @@ void Simulation::setRoom(const Room& room) {
     clear(); // Clear the scene
     // Draw the room
     addRect(0, 0, room.getLength(), room.getWidth(), QPen(Qt::black), QBrush(Qt::white));
+    // Draw the dust
+    for (const auto& dust : room.get_dirty_places())
+    {
+        QGraphicsEllipseItem* point = new QGraphicsEllipseItem(dust[0], dust[1], 1, 1);
+        point->setBrush(QBrush(Qt::black));
+        addItem(point);
+    };
     // Draw the furniture
     for (const auto& furniture : room.getFurniture()) {
         addRect(furniture.get_coordinates()[0], furniture.get_coordinates()[1],
